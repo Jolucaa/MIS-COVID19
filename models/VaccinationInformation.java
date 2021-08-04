77539878a62fec2e3829b70466353118edbf8fbb
Jolucaa <Jolucaa@gmail.com>
2021-08-04 18:31:00 +0200

    Refactor VaccinationInformation Name of value global and update in VaccineBiDose. VaccinationInformation set constructor to private like convention said.

--- models/VaccinationInformation.java
+++ models/VaccinationInformation.java
@@ -7,7 +7,11 @@ package models;
  * @version (version number or date here)
  */
 public class VaccinationInformation {
+
+    private VaccinationInformation() {
+    }
+
     public static final long DAYS_BETWEEN_VACCINE = 21;
-    public static final Integer BIDOSE_NEEDED_VACCINES = 2;
+    public static final Integer BI_DOSE_NEEDED_VACCINES = 2;
     public static final Integer UNIQUE_NEEDED_VACCINES = 1;
 }
--- models/VaccineBiDose.java
+++ models/VaccineBiDose.java
@@ -8,7 +8,7 @@ package models;
 public abstract class VaccineBiDose extends Vaccine {
     // instance variables - replace the example below with your own
     private final Long vaccinationInterval = VaccinationInformation.DAYS_BETWEEN_VACCINE;
-    private final Integer necessaryVaccines = VaccinationInformation.BIDOSE_NEEDED_VACCINES;
+    private final Integer necessaryVaccines = VaccinationInformation.BI_DOSE_NEEDED_VACCINES;
 
     /**
      * Constructor for objects of class VaccineBiDose
