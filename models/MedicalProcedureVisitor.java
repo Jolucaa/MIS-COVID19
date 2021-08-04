3ccaa9d3051348fcd67b9c6392eeb52d6a99c095
Jolucaa <Jolucaa@gmail.com>
2021-08-04 19:01:35 +0200

    Refactor interface to extend of MedicalVaccineVisitor and MedicalDiagnosticVisitor

--- models/MedicalProcedureVisitor.java
+++ models/MedicalProcedureVisitor.java
@@ -1,17 +1,4 @@
 package models;
 
-public interface MedicalProcedureVisitor{
-    void visit(Pfizer pfizer);
-
-    void visit(Moderna moderna);
-
-    void visit(Johnson johnson);
-
-    void visit(PCR pcr);
-
-    void visit(SerologicalAnalysis serologicalAnalysis);
-
-    void visit(ClassicTest classicTest);
-
-    void visit(FastTest fastTest);
+public interface MedicalProcedureVisitor extends MedicalVaccineVisitor, MedicalDiagnosticVisitor{
 }
