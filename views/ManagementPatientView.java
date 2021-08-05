83b68e48e4e38a52a9fb651de5f38016ee56f9ff
Jolucaa <Jolucaa@gmail.com>
2021-08-05 07:28:12 +0200

    Refactor switch to old style

--- views/ManagementPatientView.java
+++ views/ManagementPatientView.java
@@ -1,9 +1,6 @@
 package views;
 
 import controllers.ManagementPatientController;
-import models.Patient;
-
-import java.util.ArrayList;
 
 public class ManagementPatientView extends ClinicView {
 
@@ -14,33 +11,18 @@ public class ManagementPatientView extends ClinicView {
                 new CreatePatientView().interact(managementPatientController.getCreatePatientController());
                 break;
             case 2:
-                this.write(managementPatientController.showPatientsAvailable(), managementPatientController);
+                this.writePatientList(managementPatientController.showPatientsAvailable(), managementPatientController);
                 break;
+            default:
+                this.restart(managementPatientController);
         }
         this.getConsole().writeln();
-    }
-
-
-    protected void write(ArrayList<Patient> patientArrayList, ManagementPatientController managementPatientController) {
-        if (!patientArrayList.isEmpty()) {
-            for (Patient patient : patientArrayList) {
-                this.writeConsole(patientArrayList.listIterator().previousIndex() + " " + patient.getNameFormat() + " " + patient.getSIP());
-            }
-            Integer personSelected = this.getConsole().readInteger("Seleccione un paciente de la lista por su numero: ");
-            Patient patient = patientArrayList.get(personSelected);
-            this.setPatient(patient);
-            this.write();
-        }else{
-            this.writeConsole("No hay pacientes registrados");
-            this.restart(managementPatientController);
-        }
     }
 
     @Override
     protected void write() {
         this.writeConsole("--Gestion de Usuarios--");
-        this.writeConsole("1 Crear un Usuarios");
-        this.writeConsole("2 seleccionar un Usuarios");
-
+        this.writeConsole("1 Crear un Usuario");
+        this.writeConsole("2 Seleccionar un Usuario");
     }
 }
---
+++ views/ManagementUserView.java
@@ -0,0 +1,40 @@
+package views;
+
+import controllers.ManagementUserController;
+import utils.Console;
+
+public class ManagementUserView extends ClinicView {
+
+    public void interact(ManagementUserController managementUserController) {
+        this.write();
+        managementUserController.control();
+        switch (Console.getConsole().readInteger("Eliga una opcion: ")) {
+            case 1:
+                new CreateTechnicianView().interact(managementUserController.getCreateTechnicianController());
+                break;
+            case 2:
+                new CreateNurseView().interact(managementUserController.getCreateNurseController());
+                break;
+            case 3:
+                new ManagementPatientView().interact(managementUserController.getManagementPatientController());
+                break;
+            case 4:
+                new SwichUserView().interact(managementUserController.getSwitchUserController());
+            case 5:
+            default:
+                this.restart(managementUserController);
+                break;
+        }
+    }
+
+    @Override
+    protected void write() {
+        this.writeConsole("--Menu de administracion de usuarios--");
+        this.writeConsole("1 Crear Tecnico/a");
+        this.writeConsole("2 Crear Enfermero/a");
+        this.writeConsole("3 Gestionar Pacientes");
+        this.writeConsole("4 Cambiar Rol");
+        this.writeConsole("5 Menu Principal");
+
+    }
+}
