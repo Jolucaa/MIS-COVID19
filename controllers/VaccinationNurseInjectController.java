1832cea38e650774008ccf5425e3bcfb8599e862
Jolucaa <Jolucaa@gmail.com>
2021-08-07 12:42:18 +0200

    Refactor code to comply with the interface headers and accomplish with standard of community java.

--- controllers/VaccinationNurseInjectController.java
+++ controllers/VaccinationNurseInjectController.java
@@ -1,91 +1,41 @@
 package controllers;
 
 import models.*;
+import models.errors.ClinicError;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public class VaccinationNurseInjectController implements MedicalProcedureVisitor {
+public class VaccinationNurseInjectController implements MedicalVaccineVisitor {
 
     private Reception reception;
+
     public VaccinationNurseInjectController(Reception reception) {
         this.reception = reception;
     }
 
-    public ArrayList<MedicalProcedure> getMedicalProcessForPatient(Patient patient){
-        return reception.getMedicalProcessForPatient(patient);
+    public List<Vaccine> getVaccineCanInject(Patient patient) {
+        return reception.getVaccineCanInject(patient);
+    }
+
+    public void accept(Vaccine vaccine) {
+        vaccine.accept(this);
     }
 
     @Override
-    public boolean visit(Pfizer pfizer) {
-        return false;
-    }
+    public ClinicError visit(Pfizer pfizer) {
 
-    @Override
-    public boolean visit(Moderna moderna) {
-        return false;
-    }
-
-    @Override
-    public boolean visit(Johnson johnson) {
-        return false;
-    }
-
-    @Override
-    public boolean visit(PCR pcr) {
-        return false;
-    }
-
-    @Override
-    public boolean visit(SerologicalAnalysis serologicalAnalysis) {
-        return false;
-    }
-
-    @Override
-    public boolean visit(ClassicTest classicTest) {
-        return false;
-    }
-
-    @Override
-    public boolean visit(FastTest fastTest) {
-        return false;
-    }
-
-    @Override
-    public DiagnosticTest identify(PCR pcr) {
         return null;
     }
 
     @Override
-    public DiagnosticTest identify(SerologicalAnalysis serologicalAnalysis) {
+    public ClinicError visit(Moderna moderna) {
+
         return null;
     }
 
     @Override
-    public DiagnosticTest identify(ClassicTest classicTest) {
-        return null;
-    }
+    public ClinicError visit(Johnson johnson) {
 
-    @Override
-    public DiagnosticTest identify(FastTest fastTest) {
         return null;
-    }
-
-    @Override
-    public Vaccine vaccineFamily(Pfizer pfizer) {
-        return null;
-    }
-
-    @Override
-    public Vaccine vaccineFamily(Moderna moderna) {
-        return null;
-    }
-
-    @Override
-    public Vaccine vaccineFamily(Johnson johnson) {
-        return null;
-    }
-
-    public void accept(MedicalProcedure procedure) {
-        procedure.accept(this);
     }
 }
