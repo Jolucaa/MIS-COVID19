524ed820ef6668c722e00b72c0ee643f04e5b547
Jolucaa <Jolucaa@gmail.com>
2021-08-07 11:19:48 +0200

    Delete import of CreateNurseController

--- views/MainView.java
+++ views/MainView.java
@@ -1,7 +1,5 @@
 package views;
 
-import controllers.CreateNurseController;
-
 public interface MainView extends PresentationView, ManagementView, MedicalProceduresView, UsersViews, InformationViews, View{
 
 }
